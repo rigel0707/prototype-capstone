7f68764d5805a5ba57c2d0ac5790e6b2cf994c61
rigel0707 <espiriturigel@yahoo.com>
2023-04-21 15:12:11 +0800

    updated structure v5

--- frontend/src/components/user/Buttons.js
+++ frontend/src/components/user/Buttons.js
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import { useCookies } from 'react-cookie'
 import axios from 'axios'
 
-import apiUrl from '../components/apiUrl'
+import apiUrl from '../apiUrl'
 
 export const EditButton = ({ userId }) => {
   const [showEditModal, setShowEditModal] = useState(false)
